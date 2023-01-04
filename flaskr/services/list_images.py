031eee59185960c558444c9f8674c534d18f19ef
renantescaro <renantescaro@yahoo.com.br>
2023-01-04 12:22:08 -0300

    return tuple, list of image and number of images

--- flaskr/services/list_images.py
+++ flaskr/services/list_images.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Tuple
 from flaskr.utils.config import Config
 
 
@@ -18,18 +18,20 @@ class ListImages:
                         characters_index.append(int(image_index))
         return characters_index
 
-    def _get_images(self, characters_index:List) -> List:
+    def _get_images(self, characters_index:List) -> Tuple[List, int]:
         final_images = []
+        number_read = 0
         if path_images := Config.get('PATH_IMAGES'):
             for _, _, files in os.walk(os.path.abspath(path_images)):
                 for index, file_name in enumerate(files):
                     image_read = 's' if index in characters_index else 'n'
+                    number_read += 1 if image_read == 's' else 0
                     final_images.append({
                         'file_name':file_name,
                         'read':image_read
                     })
-        return final_images
+        return final_images, number_read
 
-    def execute(self) -> List:
+    def execute(self) -> Tuple[List, int]:
         characters_index = self._get_characters_read_index()
         return self._get_images(characters_index)
